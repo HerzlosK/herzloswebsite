9f60176cff3cbc9435cef991c3c4d5ce2a0297c4
herzlos.xw <83866695+herzlosxw@users.noreply.github.com>
2022-08-24 18:23:43 +0300

    projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,18 +2,18 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
-      name: "Code Shop",
+      image: "https://cdn.discordapp.com/attachments/964511184827088926/1012010335873544203/Ekran_goruntusu_2022-08-12_131047.png",
+      name: "MSEC DEV.",
       description:
-        "Publish your own software or images for free or for a fee. Earn money.",
-      link: "https://codeshare.me",
+        "It was a company that was established in 2020 and its purpose was to produce and sell Discord Bot Software. Msec Html, Javascript, Seashark, Python Etc. Games in Software Languages, Websites Etc. Selling Software.",
+      link: "https://msecdev.xyz",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
+      image: "https://cdn.discordapp.com/attachments/964511184827088926/1012010057875075142/24E24361-75E8-4796-A331-964CB8CDA82A.gif",
       name: "Youtube",
       description:
-        "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
+        "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 3 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
       link: "https://youtube.com/UmutBayraktarYT",
     },
      {
